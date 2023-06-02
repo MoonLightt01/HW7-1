f71e16ab379724ed4391fd15248ca2df66d81af6
MoonLightt01 <byrman.slavik@gmail.com>
2023-06-03 00:43:20 +0300

    Completed hw7 redone

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -3,17 +3,22 @@ public class Main {
         //todo Тут увольняем айтишников
 
 //        for (int i = 2; i < 101; i++) {
-//            System.out.println("Уволен айтишник Id: " + i);
+//            System.out.println("Уволен айтишник Id: " + i); //плохое решение
 //            i = i + 1;
 //        }
-
-
-
+//
+//
 //        int i = 0;
-//        while (i <= 100){
+//        while (i <= 100) {
 //            System.out.println("Уволен айтишник Id: " + i);
 //            i = i + 2;
 //        }
+
+        for(int i = 2;i <= 100; i = i + 2) {
+
+            System.out.println("Уволен айтишник id:" + i);
+        }
+
 
 
 
